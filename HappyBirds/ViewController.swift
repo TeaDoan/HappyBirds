51352a7d5d130b046277cec16bc8cf00623987a8
TeaDoan <TeaDoan@github.com>
2018-07-21 20:00:43 -0600

    Initial Commit

---
+++ HappyBirds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HappyBirds
+//
+//  Created by Thao Doan on 7/21/18.
+//  Copyright © 2018 Thao Doan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
